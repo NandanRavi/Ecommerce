1f8cf208226d8c5503e0f1951739a709fc0a26ba
NandanRavi <monubibhu@gmail.com>
2024-11-11 17:08:56 +0530

    product html with enctype

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'images/'
+MEDIA_URL = ''
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
